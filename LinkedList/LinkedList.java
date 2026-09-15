d9072a71cb4a66fa7ff591788f5a1e4d5be1fc5c
VenkataRatnamOleti <venkataratnamoleti@gmail.com>
2026-09-15 13:28:53 +0530

    LinkedList - Print Completed

---
+++ LinkedList/LinkedList.java
@@ -0,0 +1,59 @@
+public class LinkedList{
+    public static class Node{
+        int data;
+        Node next;
+
+        public Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static Node head;
+    public static Node tail;
+
+    public void addFirst(int data){
+        // Node newNode = new Node(data);
+        // newNode.next = head;
+        // head = newNode;
+        // if(tail == null){
+        //     tail = newNode;
+        // }
+
+        Node newNode = new Node(data);
+        if(head == null){
+            head = tail = newNode;
+            return;
+        }
+        newNode.next = head;
+        head = newNode;
+    }
+
+    public void addLast(int data){
+        Node newNode = new Node(data);
+        if(head == null){
+            head = tail = newNode;
+            return;
+        }
+        tail.next = newNode;
+        tail = newNode;
+    }
+
+    public void print(){
+        Node temp = head;
+        while(temp != null){
+            System.out.print(temp.data+" ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        LinkedList ll = new LinkedList();
+        ll.addFirst(1);
+        ll.addLast(2);
+        ll.addLast(3);
+        ll.addLast(4);
+        ll.print();
+    }
+}
